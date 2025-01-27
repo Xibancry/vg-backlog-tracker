2470c0f5d4ca54cc676aa6b3059a1d1f32af0cd7
Xibancry <jakboss43@gmail.com>
2025-01-27 10:43:13 +0100

    Modified HomePage

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -6,18 +6,19 @@ function HomePage() {
     <div className="container-fluid py-5">
       {/* Hero Section */}
       <div className="px-4 py-5 my-5 text-center">
-        <h1 className="display-5 fw-bold">Welcome to Your Site</h1>
+        <h1 className="display-5 fw-bold">Welcome to VGCT</h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4">
-            Quickly design and customize responsive mobile-first sites with Bootstrap,
-            the world's most popular front-end open source toolkit.
+            Keep track of your videogames collection, create lists,
+            manage your backlog, and compare achievements with your friends.<br/>
+            All in one webpage!
           </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
-              Primary Action
+              Register
             </button>
             <button type="button" className="btn btn-outline-secondary btn-lg px-4">
-              Secondary Action
+              Log in
             </button>
           </div>
         </div>
