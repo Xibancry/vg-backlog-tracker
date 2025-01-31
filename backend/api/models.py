737d897bfc3ddade2d34b63fd152987361728198
Xibancry <jakboss43@gmail.com>
2025-01-31 12:41:03 +0100

    Test migration

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    nickname = models.CharField(max_length=20)
+    email = models.EmailField()
+    date_of_birth = models.DateField()
+    password = models.CharField(max_length=256)
