fa9d33ceef9a97fca2813dfc3fd1653fb203fb41
Xibancry <jakboss43@gmail.com>
2025-02-03 12:36:10 +0100

    Created additional models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class User(models.Model):
@@ -6,3 +7,11 @@ class User(models.Model):
     email = models.EmailField()
     date_of_birth = models.DateField()
     password = models.CharField(max_length=256)
+    member_since = models.DateField(default=date.today)
+
+class Collection(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    game_id = models.IntegerField()
+    is_completed = models.SmallIntegerField() #0: Backlog, 1: In progress, 2: Beaten, 3:Completionist
+    time_played = models.DurationField()
+    
