ddc62b7e03f11e678ed5ff193fa9162759575aff
Xibancry <jakboss43@gmail.com>
2025-02-03 12:31:14 +0100

    Added sign in page

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -16,7 +16,8 @@ function HomePage() {
             All in one webpage!
           </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
+            <button type="button" className="btn btn-primary btn-lg px-4 gap-3"
+            onClick={() => navigate('signup')}>
               Register
             </button>
             <button type="button" className="btn btn-outline-secondary btn-lg px-4"
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import HomePage from './components/HomePage.jsx';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import LoginPage from './components/LoginPage.jsx';
+import SignUpPage from './components/SignUpPage.jsx';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import {BrowserRouter as Router, Route, Link, useNavigate, BrowserRouter, Routes } from 'react-router-dom';
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<><Header /><HomePage /><Footer /></>} />
         <Route path="/LoginPage" element={<><Header /><LoginPage /><Footer /></>} />
+        <Route path="/signup" element={<><Header /><SignUpPage /><Footer /></>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
